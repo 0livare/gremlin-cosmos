4514c0a2c0d9d0148a323b72078c4b95f2d43f5a
Zach Posten <zach@posten.io>
2020-07-19 18:37:50 -0500

    chore: Switch build from ES to CJS
    This code is intended to be run by node, which uses require() syntax
    not ES modules import syntax.

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,9 @@ import typescript from '@rollup/plugin-typescript'
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'dist', // This is also declared in tsconfig
-    format: 'es', // Output bundle should use ES Module syntax
+    dir: 'dist',
+    format: 'cjs',
     sourcemap: true,
-    exports: 'named', // Export multiple named modules instead of a single default one
   },
   // Do not include any 3rd party libraries in the bundled code
   external: id => !id.startsWith('.') && !path.isAbsolute(id),
