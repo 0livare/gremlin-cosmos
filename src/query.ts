cf60a883e0e991c4ace545d7662f65ba8116c2ec
Zach Posten <zach@posten.io>
2020-10-05 18:26:51 -0500

    fix(query): Don't allow null property values

--- src/query.ts
+++ src/query.ts
@@ -353,7 +353,15 @@ export class Query {
   // Side effect steps
   /////////////////////
 
-  /** Add a property to an element. */
+  /** 
+   * Add a property to an element.
+   * 
+   * An object can optionally be passed to this function.
+   * Each key-value pair that exists in the object will 
+   * be added as if it were called with `.property(key, value)`
+   * unless the value is `null` or `undefined`, in which 
+   * case it will not be added.
+   */
   property(key: string | Index, value?: string) {
     if (typeof key === 'string') {
       this.query += `.property(${quoteAndCombine(key, value)})`
@@ -361,8 +369,12 @@ export class Query {
     }
 
     let map: Index = key
-    for (let k in map) {
-      this.property(k, map[k])
+    if (typeof map !== 'object' || map === null || Array.isArray(map)) return this
+
+    for (let key in map) {
+      let value = map[key]
+      if (value === null || value === undefined) continue
+      this.property(key, value)
     }
 
     return this
