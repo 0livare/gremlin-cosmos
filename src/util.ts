f9885344f96c8fd8a468be735820d6e456e1ad71
amidha <amit.dhawan@barco.com>
2021-06-06 14:30:30 +0530

    Fixed bug to support empty string in value

--- src/util.ts
+++ src/util.ts
@@ -2,13 +2,13 @@ export function isAPromise(obj: any): boolean {
   return obj.then && typeof obj.then === 'function'
 }
 
-export function quoteAndCombine(...args) {
+export function quoteAndCombine(...args: (string | number | boolean | any[])[]) {
   if (Array.isArray(args[0])) {
     args = args[0]
   }
 
   return args
-    .filter(arg => arg)
-    .map(arg => `'${arg}'`)
+    .filter((arg) => arg === '' ? `''` : arg)
+    .map(arg => arg === '' ? `''` : `'${arg}'`)
     .join(', ')
 }
